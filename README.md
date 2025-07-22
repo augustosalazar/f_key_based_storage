f3fd83d12a43f7a129e9365f4a4feaeb4e4e1ee1
Augusto Salazar SIlva <augustosalazar@uninorte.edu.co>
2025-07-22 13:58:30 -0500

    Update readme and integration test

--- README.md
+++ README.md
@@ -1,10 +1,15 @@
-# f_shared_prefs
+# f_key_based_storage
 
-Implementation of the f_shared_prefs_template
+Implementation of the f_key_based_storage_template
 
-This is a Flutter project that demonstrates the use of shared preferences for a single user authentication flow. It includes a login page, home page, and uses GetX for state management.
+This is a Flutter project that demonstrates the use of key based storage with:
+
+- [Shared prefs](https://pub.dev/packages/shared_preferences) Shared preferences
+- [Secured storage](https://pub.dev/packages/flutter_secure_storage)
+ 
+For a single user authentication flow. It includes a login page, home page using GetX for state management.
 
 <img src="https://user-images.githubusercontent.com/4458129/178773695-b5a7aa7d-fce4-4e72-952a-384c0f6ce924.gif" width="300" />
 
 Run the integration test with:  
-flutter test integration_test
+flutter test integration_test (include both implementations)
--- integration_test/app_test.dart
+++ integration_test/app_test.dart
@@ -13,10 +13,21 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:get/get.dart';
 import 'package:integration_test/integration_test.dart';
 
-Future<Widget> createHomeScreen() async {
+Future<Widget> createHomeScreenSecured() async {
   WidgetsFlutterBinding.ensureInitialized();
-  //Get.put<ILocalPreferences>(LocalPreferencesShared());
+  Get.deleteAll();
   Get.put<ILocalPreferences>(LocalPreferencesSecured());
+  Get.put<ILocalAuthSource>(SharedPrefLocalAuthSource());
+  Get.put<IAuthRepo>(AuthRepo(Get.find()));
+  Get.put(AuthUseCase(Get.find()));
+  Get.put(AuthController(Get.find()));
+  return const MyApp();
+}
+
+Future<Widget> createHomeScreenShared() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  Get.deleteAll();
+  Get.put<ILocalPreferences>(LocalPreferencesShared());
   Get.put<ILocalAuthSource>(SharedPrefLocalAuthSource());
   Get.put<IAuthRepo>(AuthRepo(Get.find()));
   Get.put(AuthUseCase(Get.find()));
@@ -27,8 +38,9 @@ Future<Widget> createHomeScreen() async {
 void main() {
   IntegrationTestWidgetsFlutterBinding.ensureInitialized();
 
-  testWidgets("Complete Authentication flow", (WidgetTester tester) async {
-    Widget w = await createHomeScreen();
+  testWidgets("Complete Authentication flow with shared",
+      (WidgetTester tester) async {
+    Widget w = await createHomeScreenShared();
     await tester.pumpWidget(w);
 
     await tester.pumpAndSettle(Durations.extralong4);
@@ -53,6 +65,7 @@ void main() {
     await tester.tap(find.byKey(const Key('signUpSubmit')));
 
     await tester.pumpAndSettle(Durations.extralong4);
+    await tester.pumpAndSettle(Durations.extralong4);
 
     //expect(find.text('User ok'), findsOneWidget);
 
@@ -76,6 +89,82 @@ void main() {
 
     await tester.pumpAndSettle(Durations.extralong4);
     await tester.pumpAndSettle(Durations.extralong4);
+
+    //verify that we are in content page
+    expect(find.byKey(const Key('contentScaffold')), findsOneWidget);
+
+    // go to profile page
+    await tester.tap(find.byIcon(Icons.verified_user));
+    await tester.pumpAndSettle();
+
+    // logout
+    await tester.tap(find.byKey(const Key('profileLogout')));
+    await tester.pumpAndSettle();
+
+    //verify that we are in login page
+    expect(find.byKey(const Key('loginScaffold')), findsOneWidget);
+
+    await tester.enterText(find.byKey(const Key('loginEmail')), 'a@a.com');
+
+    await tester.enterText(find.byKey(const Key('loginPassword')), '123456');
+
+    await tester.tap(find.byKey(const Key('loginSubmit')));
+
+    await tester.pumpAndSettle(Durations.extralong4);
+
+    //verify that we are in content page
+    expect(find.byKey(const Key('contentScaffold')), findsOneWidget);
+  });
+
+  testWidgets("Complete Authentication flow with secured",
+      (WidgetTester tester) async {
+    Widget w = await createHomeScreenSecured();
+    await tester.pumpWidget(w);
+
+    await tester.pumpAndSettle(Durations.extralong4);
+
+    //verify that we are in login page
+    expect(find.byKey(const Key('loginScaffold')), findsOneWidget);
+
+    expect(find.byKey(const Key('loginEmail')), findsNWidgets(1));
+
+    // go to sign in
+    await tester.tap(find.byKey(const Key('loginCreateUser')));
+
+    await tester.pumpAndSettle(Durations.extralong4);
+
+    //verify that we are in signup page
+    expect(find.byKey(const Key('signUpScaffold')), findsOneWidget);
+
+    await tester.enterText(find.byKey(const Key('signUpEmail')), 'a@a.com');
+
+    await tester.enterText(find.byKey(const Key('signUpPassword')), '123456');
+
+    await tester.tap(find.byKey(const Key('signUpSubmit')));
+
+    await tester.pumpAndSettle(Durations.extralong4);
+    await tester.pumpAndSettle(Durations.extralong4);
+
+    //expect(find.text('User ok'), findsOneWidget);
+
+    //verify that we are in login page
+    expect(find.byKey(const Key('loginScaffold')), findsOneWidget);
+    //login
+
+    await tester.enterText(find.byKey(const Key('loginEmail')), 'b@b.com');
+
+    await tester.enterText(find.byKey(const Key('loginPassword')), '123456');
+
+    await tester.tap(find.byKey(const Key('loginSubmit')));
+
+    await tester.pumpAndSettle(Durations.extralong4);
+
+    await tester.enterText(find.byKey(const Key('loginEmail')), 'a@a.com');
+
+    await tester.enterText(find.byKey(const Key('loginPassword')), '123456');
+
+    await tester.tap(find.byKey(const Key('loginSubmit')));
+
     await tester.pumpAndSettle(Durations.extralong4);
     await tester.pumpAndSettle(Durations.extralong4);
 
